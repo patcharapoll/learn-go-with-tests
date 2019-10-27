7d93084f09744f567845ecadea6801714c8571a7
Patcharapol Chartvivatpornchai <patcharapol@codefin.io>
2019-10-27 20:09:17 +0700

    hello test constance

---
+++ path1/hello-world/v4/hello.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+
+// Hello returns greeting
+func Hello(name string) string {
+	return englishHelloPrefix + name
+}
+
+func main() {
+	fmt.Println(Hello("world"))
+}
